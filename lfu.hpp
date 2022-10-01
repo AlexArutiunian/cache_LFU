cf1d82bd851a0d18b482cfe36560b5108ef45bd3
Alexandr Arutiunian <89289600+AlexArutiunian@users.noreply.github.com>
2022-10-01 15:08:31 +0300

    Update lfu.hpp

--- lfu.hpp
+++ lfu.hpp
@@ -8,17 +8,13 @@
 #include <algorithm>
 
 
-template <typename T = int> struct page{
+template <typename T = int> struct page {
     T id;
     int freq;
-    page(){ freq = 0; }
-    
-    
+    page(){ freq = 0; }  
 };
 
-namespace caches {
-
-    
+namespace caches {  
 template <typename T = int, typename KeyT = page<int>> struct cache_t{
 
     size_t sz_;
@@ -30,18 +26,15 @@ template <typename T = int, typename KeyT = page<int>> struct cache_t{
 
     bool full() const { return (cache_.size() == sz_); }
     
-    bool lookup_update(KeyT key) {
-        
+    bool lookup_update(KeyT key) {    
         auto it = hash_.find(key.id);
         if(it == hash_.end()){            
             
             if(full()){                        
-                
                 auto i = cache_.begin();
                 auto kick_it = hash_.find(i->id);  
                 hash_.erase(kick_it);
-                cache_.pop_front();
-                
+                cache_.pop_front();         
             }
 
             key.freq += 1;      
@@ -49,12 +42,10 @@ template <typename T = int, typename KeyT = page<int>> struct cache_t{
             hash_[key.id] = cache_.begin();
             for(auto i = cache_.begin(); i != cache_.end(); i++) std::cout << i->id << " ";
             std::cout << " " << std::endl;
-            return false;
-              
+            return false;          
         }
                
-        else{
-        
+        else{     
             (it->second)->freq += 1;
             auto i = cache_.begin();                         // save freq sort in cache_
             auto kick = cache_.begin();
@@ -70,9 +61,8 @@ template <typename T = int, typename KeyT = page<int>> struct cache_t{
             hash_[((it->second)->id)] = itr;
              
             return true;
-        } 
-        
-        }
+        }   
+    }
        
     };
 }
