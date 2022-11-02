e3c698809a4ba6ba602074a2471d03054c73dbd1
AlexArutiunian <arutunanalexandr17@gmail.com>
2022-11-02 17:49:47 +0300

    input in ideal

--- ideal.cpp
+++ ideal.cpp
@@ -12,17 +12,17 @@ int main(){
     int num_pages = 10;
     size_t capacity = 3;
     
-   // std::cin >> capacity >> num_pages;
+    std::cin >> capacity >> num_pages;
 
     int count = 0;  
     ideal_caches::ideal_cache_<T> c{capacity};
     std::list<T> all_keys;
-    /*for(int i = 0; i != num_pages; i++){
+    for(int i = 0; i != num_pages; i++){
         T k;
         std::cin >> k;
         all_keys.push_back(k);
-    }*/
-    all_keys = {4, 1, 7, 1, 1, 1, 7, 0, 4, 0};
+    }
+  //  all_keys = {4, 1, 7, 1, 1, 1, 7, 0, 4, 0};
 
     for(auto i = all_keys.begin(); i != all_keys.end(); ++i){
         
